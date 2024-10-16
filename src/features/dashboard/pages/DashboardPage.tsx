f9d63b2344181d0360202e64400c4b6af9382a0d
Dwi Fitriana <cc2039506@gmail.com>
2024-10-16 13:26:47 +0700

    add number

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -56,11 +56,17 @@ const DashboardPage = () => {
           <CardContent className='p-0 flex justify-evenly gap-4'>
             {kontl.map(() => (
               <div className={cn('flex items-center gap-4 justify-center', { 'flex-col': kontl.length > 3 })}>
-                <img
-                  alt='paslon'
-                  src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
-                  className='rounded-xl max-w-[110px] aspect-square object-cover border-[3px] border-blue-400'
-                />
+                <div className='relative'>
+                  <img
+                    alt='paslon'
+                    src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
+                    draggable={false}
+                    className='rounded-xl aspect-square object-cover border-[3px] border-blue-400'
+                  />
+                  <div className='absolute bottom-2 right-2 bg-blue-400 text-white size-5 rounded text-xs flex items-center justify-center'>
+                    1
+                  </div>
+                </div>
                 <div className={cn('space-y-1', { 'text-center': kontl.length > 3 })}>
                   <div>
                     <h1 className='font-bold text-3xl text-blue-400'>45,30%</h1>
