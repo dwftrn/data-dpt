7440cc415dbb6929daee953e69e90194e6388375
Dwi Fitriana <cc2039506@gmail.com>
2024-10-21 14:10:58 +0700

    draggable false

--- src/features/pemilu/pages/PemiluPage.tsx
+++ src/features/pemilu/pages/PemiluPage.tsx
@@ -32,6 +32,7 @@ const PemiluPage = () => {
                 <img
                   src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
                   alt='avatar'
+                  draggable={false}
                   className='size-20 lg:size-36 aspect-square rounded-full border-4 lg:border-8 border-white drop-shadow-lg'
                 />
                 <div className='rounded-full size-6 lg:size-10 flex items-center justify-center absolute bottom-0 bg-red-500 text-white'>
@@ -42,6 +43,7 @@ const PemiluPage = () => {
                 <img
                   src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
                   alt='avatar'
+                  draggable={false}
                   className='size-20 lg:size-36 aspect-square rounded-full border-4 lg:border-8 border-white drop-shadow-lg'
                 />
                 <div className='rounded-full size-6 lg:size-10 flex items-center justify-center absolute bottom-0 bg-red-500 text-white'>
@@ -52,6 +54,7 @@ const PemiluPage = () => {
                 <img
                   src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
                   alt='avatar'
+                  draggable={false}
                   className='size-20 lg:size-36 aspect-square rounded-full border-4 lg:border-8 border-white drop-shadow-lg'
                 />
                 <div className='rounded-full size-6 lg:size-10 flex items-center justify-center absolute bottom-0 bg-red-500 text-white'>
@@ -62,6 +65,7 @@ const PemiluPage = () => {
                 <img
                   src='https://s3-alpha-sig.figma.com/img/a75f/5557/188808cc185497e930c104977af4ebae?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kNwjA6h3yJHdeNJ2ZsSx~lvpboPoxkV-tZTEJYhhSVrHt-FtzR0Slmywio2FgM4THz2GhbRHbs7mdx4HBThX8s3XOPbSDnu9B0uoue7EoaNcvFclQAj9KuYKiB0hZ8V2avk4cfS0xv5hH7qmRtQYr-SovTIe~nXyUFiu0t0ONw881Scy70c9yLx3kDAdnWO8KM49b0aA6y0TskBPMBEtSBdtXHxNCU-HM~rHhFeRAthD~Y-vF9pBDk3wfAXV8AW5y3ETSAmIKBSLcJZhuHlg6qPzH-vIiEE2Dbq-YN52w69OSux9z1qohSDBmwiNVGh3Yi2ace0xeSuqL0GhYBaFPA__'
                   alt='avatar'
+                  draggable={false}
                   className='size-20 lg:size-36 aspect-square rounded-full border-4 lg:border-8 border-white drop-shadow-lg'
                 />
                 <div className='rounded-full size-6 lg:size-10 flex items-center justify-center absolute bottom-0 bg-red-500 text-white'>
