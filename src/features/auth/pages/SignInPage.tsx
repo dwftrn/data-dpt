595884d28d850b20e8e74b7ef73a803d6323ffb7
Dwi Fitriana <cc2039506@gmail.com>
2024-11-08 14:22:47 +0700

    change logo

--- src/features/auth/pages/SignInPage.tsx
+++ src/features/auth/pages/SignInPage.tsx
@@ -1,12 +1,12 @@
 import SignInForm from '../components/SignInForm'
-import Logo from '@/assets/logo-horizontal.svg'
+import Logo from '@/assets/logo-vertical.svg'
 
 const SignInPage = () => {
   return (
     <div className='flex h-dvh w-dvw items-center justify-center p-8 bg-primary relative'>
       <div className='h-3/5 w-full absolute bg-primary-blue-300 top-0'></div>
       <div className='z-50 flex flex-col items-center justify-center gap-6'>
-        <img src={Logo} alt='LOGO' className='z-50 w-[150px]' />
+        <img src={Logo} alt='LOGO' draggable={false} className='z-50 w-[100px]' />
         <SignInForm />
       </div>
     </div>
