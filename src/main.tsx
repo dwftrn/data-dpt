a19417d1993af9a1cfec03310b9c7b3bad55057d
Dwi Fitriana <cc2039506@gmail.com>
2024-11-11 16:45:31 +0700

    add suspense

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { Toaster } from './components/ui/sonner.tsx'
 import { TooltipProvider } from './components/ui/tooltip.tsx'
 import './index.css'
 import { router } from './router.tsx'
+import { Suspense } from 'react'
+import LoadingOverlay from './components/LoadingOverlay.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <TanstackProvider>
-    <TooltipProvider>
-      <RouterProvider router={router} />
-      <Toaster richColors toastOptions={{}} />
-    </TooltipProvider>
+    <Suspense fallback={<LoadingOverlay />}>
+      <TooltipProvider>
+        <RouterProvider router={router} />
+        <Toaster richColors toastOptions={{}} />
+      </TooltipProvider>
+    </Suspense>
   </TanstackProvider>
 )
