8047c34ff4fb08d47aac2e9749351a6dbfe60a61
Dwi Fitriana <cc2039506@gmail.com>
2024-11-16 14:47:30 +0700

    add tooltip

--- src/features/dashboard/components/DashboardDesktop.tsx
+++ src/features/dashboard/components/DashboardDesktop.tsx
@@ -10,6 +10,7 @@ import VoteStatisticsCardList from '../components/VoteStatisticsCardList'
 import useFetchQuickCount from '../queries/useFetchQuickCount'
 import CandidateCardSkeleton from './CandidateCardSkeleton'
 import MapContainer from './MapContainer'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 
 const calcPercentage = (total: number, count: number) => {
   return (count * 100) / total
@@ -48,28 +49,81 @@ const DashboardDesktop = () => {
             {quickCount && (
               <div className='flex items-center w-full h-[50px] [&>div:first-child]:rounded-l-xl [&>div:last-child]:rounded-r-xl rounded-xl border-grey-500 border'>
                 {quickCount.calon_hasil.map((item) => (
-                  <div
-                    key={item.id_paslon}
-                    style={{
-                      backgroundColor: item.warna,
-                      width:
-                        calcPercentage(quickCount.total_dpt - quickCount.total_suara_tidak_sah, item.jumlah_suara) +
-                        '%',
-                      height: '100%'
-                    }}
-                  ></div>
+                  <Tooltip key={item.id_paslon}>
+                    <TooltipTrigger asChild>
+                      <div
+                        style={{
+                          backgroundColor: item.warna,
+                          width:
+                            calcPercentage(quickCount.total_dpt - quickCount.total_suara_tidak_sah, item.jumlah_suara) +
+                            '%',
+                          height: '100%'
+                        }}
+                      ></div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <div className='grid items-center grid-cols-[3px_1fr] gap-2'>
+                        <div className='h-full rounded-lg' style={{ backgroundColor: item.warna }}></div>
+                        <div className='flex flex-col gap-1'>
+                          <p>
+                            {item.nama} & {item.nama_vice}
+                          </p>
+                          <p>
+                            {Number(item.jumlah_suara).toLocaleString('id')}{' '}
+                            <span className='font-medium'>
+                              (
+                              {Number(
+                                calcPercentage(
+                                  quickCount.total_dpt - quickCount.total_suara_tidak_sah,
+                                  item.jumlah_suara
+                                )
+                              ).toLocaleString('id', { maximumFractionDigits: 2 })}{' '}
+                              % )
+                            </span>
+                          </p>
+                        </div>
+                      </div>
+                    </TooltipContent>
+                  </Tooltip>
                 ))}
 
-                <div
-                  className='bg-grey-500 h-full'
-                  style={{
-                    width:
-                      calcPercentage(
-                        quickCount.total_dpt - quickCount.total_suara_tidak_sah,
-                        quickCount.total_dpt - quickCount.total_suara_tidak_sah - quickCount.total_suara_masuk
-                      ) + '%'
-                  }}
-                ></div>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <div
+                      className='bg-grey-500 h-full'
+                      style={{
+                        width:
+                          calcPercentage(
+                            quickCount.total_dpt - quickCount.total_suara_tidak_sah,
+                            quickCount.total_dpt - quickCount.total_suara_tidak_sah - quickCount.total_suara_masuk
+                          ) + '%'
+                      }}
+                    ></div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <div className='grid items-center grid-cols-[3px_1fr] gap-2'>
+                      <div className='h-full rounded-lg bg-grey-500'></div>
+                      <div className='flex flex-col gap-1'>
+                        <p>Suara Belum Masuk</p>
+                        <p>
+                          {Number(
+                            quickCount.total_dpt - quickCount.total_suara_tidak_sah - quickCount.total_suara_masuk
+                          ).toLocaleString('id')}{' '}
+                          <span className='font-medium'>
+                            (
+                            {Number(
+                              calcPercentage(
+                                quickCount.total_dpt - quickCount.total_suara_tidak_sah,
+                                quickCount.total_dpt - quickCount.total_suara_tidak_sah - quickCount.total_suara_masuk
+                              )
+                            ).toLocaleString('id', { maximumFractionDigits: 2 })}{' '}
+                            % )
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </TooltipContent>
+                </Tooltip>
               </div>
             )}
 
